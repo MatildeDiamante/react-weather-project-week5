c1bb52271d2fb02593ac6ae9d4643970819869c5
Matilde Moretti <matilde.moretti95@gmail.com>
2023-11-23 20:46:25 +0100

    Added GitHub repository url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is created by Matilde Moretti and is{" "}
+        <a
+          href="https://github.com/MatildeDiamante/react-weather-project-week5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
